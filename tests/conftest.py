8218c204a08820efb6eccfdf578031ffc003a503
Aramchik <Aramchik@users.noreply.github.com>
2026-02-17 18:10:59 +0300

    Update conftest.py
    add:
    options.add_argument("--headless=new")  
    options.add_argument("--disable-gpu")
    options.add_argument("--no-sandbox")    
    options.add_argument("--disable-dev-shm-usage")
    options.add_argument("--window-size=1920,1080")  
    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,11 +6,13 @@ from selenium.webdriver.chrome.options import Options
 @pytest.fixture()
 def browser():
     options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
-    options.add_experimental_option('detach', True)
+    options.add_argument("--headless=new")  # новый headless, более стабильный
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")    # обязательно в CI
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")  # фиксируем размер окна
+    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
     chrome_browser = webdriver.Chrome(options=options)
-    chrome_browser.maximize_window()
-    chrome_browser.implicitly_wait(10)
     return chrome_browser
 
 
