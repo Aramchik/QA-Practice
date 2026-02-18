6cc0bf2b2a1acf88fe09544db1671f350aa99543
Aramchik <Aramchik@users.noreply.github.com>
2026-02-18 07:24:18 +0300

    Update text_area.py

--- pages/text_area.py
+++ pages/text_area.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as Ec
 from pages.base import BasePage
-
+import time
 
 # Lockators
 submit_button = (By.XPATH, "//input[@id='submit-id-submit']")
