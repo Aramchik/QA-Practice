120dfb2c38e44be25a5afa5722979b2955bc634d
Aramchik <Aramchik@users.noreply.github.com>
2026-02-22 09:33:21 +0300

    Update test_text_area.py

--- tests/test_text_area.py
+++ tests/test_text_area.py
@@ -18,7 +18,7 @@ def test_text_area(browser):
     with allure.step('Click submit and assert'):
         ta.click_submit()
         ta.do_screenshot()
-        ta.assert_text_area_input()
+        ta.assert_values()
     with allure.step('Do screenshot'):
         ta.do_screenshot()
 
